a11f4388b6e8e3281792cf80849b83f2aa027a84
Dima Karpovich <karpovich.code@gmail.com>
2018-08-15 21:25:22 +0300

    FIXING: CONVERTERS

--- src/main/java/com/basakdm/excartest/utils/ConvertOrders.java
+++ src/main/java/com/basakdm/excartest/utils/ConvertOrders.java
@@ -5,6 +5,7 @@ import com.basakdm.excartest.entity.OrderEntity;
 import com.basakdm.excartest.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Date;
 import java.util.Optional;
 
 public class ConvertOrders {
@@ -14,11 +15,16 @@ public class ConvertOrders {
 
     public static OrderDTO mapOrder(OrderEntity orderEntity) {
         OrderDTO orderDTO = new OrderDTO();
-        orderDTO.setFinPrice(orderEntity.getFinPrice());
+
         orderDTO.setId(orderEntity.getId());
-        orderDTO.setAmount_of_days(orderEntity.getAmount_of_days());
-        orderDTO.setFrom_what_date(orderEntity.getFrom_what_date());
         orderDTO.setPriceAdd(orderEntity.getPriceAdd());
+        orderDTO.setFinPrice(orderEntity.getFinPrice());
+        orderDTO.setFrom_what_date(orderEntity.getFrom_what_date());
+        orderDTO.setId_car(orderEntity.getId_car());
+        orderDTO.setId_user(orderEntity.getId_user());
+        orderDTO.setId_owner(orderEntity.getId_owner());
+        orderDTO.setAmount_of_days(orderEntity.getAmount_of_days());
+
         return orderDTO;
     }
 
@@ -29,11 +35,15 @@ public class ConvertOrders {
         if (optionalOrderEntity.isPresent()) orderEntity = optionalOrderEntity.get();
         else throw new RuntimeException("Incorrect ID of car");
 
-        orderEntity.setFinPrice(orderDTO.getFinPrice());
         orderEntity.setId(orderDTO.getId());
-        orderEntity.setAmount_of_days(orderDTO.getAmount_of_days());
-        orderEntity.setFrom_what_date(orderDTO.getFrom_what_date());
         orderEntity.setPriceAdd(orderDTO.getPriceAdd());
+        orderEntity.setFinPrice(orderDTO.getFinPrice());
+        orderEntity.setFrom_what_date(orderDTO.getFrom_what_date());
+        orderEntity.setId_car(orderDTO.getId_car());
+        orderEntity.setId_user(orderDTO.getId_user());
+        orderEntity.setId_owner(orderDTO.getId_owner());
+        orderEntity.setAmount_of_days(orderDTO.getAmount_of_days());
+
         return orderEntity;
     }
 }
