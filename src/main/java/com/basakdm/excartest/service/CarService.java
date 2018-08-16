2a8f3b86fe2f2dd8c756454f8c547a1ebce993e5
Dima Karpovich <karpovich.code@gmail.com>
2018-08-16 09:19:33 +0300

    DOCS for SERVICES

--- src/main/java/com/basakdm/excartest/service/CarService.java
+++ src/main/java/com/basakdm/excartest/service/CarService.java
@@ -1,7 +1,6 @@
 package com.basakdm.excartest.service;
 
 
-import com.basakdm.excartest.dto.CarDTO;
 import com.basakdm.excartest.entity.CarEntity;
 import com.basakdm.excartest.enum_ent.car_enum.*;
 import org.springframework.stereotype.Service;
@@ -14,14 +13,12 @@ public interface CarService  {
 
     /**
      * Get all cars.
-     *
      * @return collection of cars.
      */
     Collection<CarEntity> findAll();
 
     /**
      * Find car by id
-     *
      * @param id car unique identifier.
      * @return Optional with car, if car was founded. Empty optional in opposite case.
      */
@@ -41,14 +38,57 @@ public interface CarService  {
      */
     void delete(long id);
 
+    /**
+     * Update car by id.
+     * @param carEntity car params for update a car.
+     * @return  Void.
+     */
     void update(CarEntity carEntity);
 
+    /**
+     * Find cars by (isActivated = false).
+     * @return  Collection<CarEntity>.
+     */
     Collection<CarEntity> findAllByIsActivatedFalse();
+
+    /**
+     * Find cars by (isActivated = true).
+     * @return  Collection<CarEntity>.
+     */
     Collection<CarEntity> findAllByIsActivatedTrue();
 
+    /**
+     * Find cars by transmission type.
+     * @param transmission car params to give out a list of cars with such a transmission.
+     * @return  Collection<CarEntity>.
+     */
     Collection<CarEntity> findAllByTransmissionType(Transmission transmission);
+
+    /**
+     * Find cars by car body.
+     * @param carBody car params to give out a list of cars with such a car body.
+     * @return  Collection<CarEntity>.
+     */
     Collection<CarEntity> findAllByCarBody(CarBody carBody);
+
+    /**
+     * Find cars by drive gear.
+     * @param driveGear car params to give out a list of cars with such a drive gear.
+     * @return  Collection<CarEntity>.
+     */
     Collection<CarEntity> findAllByDriveGear(DriveGear driveGear);
+
+    /**
+     * Find cars by type engine.
+     * @param typeEngine car params to give out a list of cars with such a type engine.
+     * @return  Collection<CarEntity>.
+     */
     Collection<CarEntity> findAllByEngineType(TypeEngine typeEngine);
+
+    /**
+     * Find cars by type fuel.
+     * @param typeFuel car params to give out a list of cars with such a type fuel.
+     * @return  Collection<CarEntity>.
+     */
     Collection<CarEntity> findAllByFuelType(TypeFuel typeFuel);
 }
--- src/main/java/com/basakdm/excartest/service/UserService.java
+++ src/main/java/com/basakdm/excartest/service/UserService.java
@@ -1,7 +1,5 @@
 package com.basakdm.excartest.service;
 
-import com.basakdm.excartest.dto.UserDTO;
-import com.basakdm.excartest.entity.CarEntity;
 import com.basakdm.excartest.entity.UserEntity;
 import org.springframework.stereotype.Service;
 
@@ -33,7 +31,17 @@ public interface UserService {
      */
     UserEntity createUser(UserEntity userEntity);
 
+    /**
+     * Delete user by id.
+     * @param id user params for delete a user.
+     * @return  Void.
+     */
     void delete(long id);
 
+    /**
+     * Update users by id.
+     * @param userEntity user params for update a users.
+     * @return  Void.
+     */
     void update(UserEntity userEntity);
 }
